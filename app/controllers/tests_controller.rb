66c43536e465a98c433378febe6faac440ef8688
ruuke <ruuke@mail.ru>
2019-03-22 16:34:19 +0500

    added show resources

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -2,10 +2,16 @@ class TestsController < Simpler::Controller
 
   def index
     @time = Time.now
+    render plain: 'Plain text response'
+    status 201
   end
 
   def create
 
+  end
+
+  def show
+    @test_id = params[:id]
   end
 
 end
